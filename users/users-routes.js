9a13c8c56cbea5f0e4d1304b752ed16b5fda5f2f
Danielle Custodio <dani.ps.custodio@gmail.com>
2019-08-29 19:08:26 -0300

    add auth route

--- users/users-routes.js
+++ users/users-routes.js
@@ -9,7 +9,19 @@ const register = (req, res, next) => {
 		.catch(err => next(err))
 }
 
+const auth = (req, res, next) => {
+	userService
+		.auth(req.body)
+		.then(user =>
+			user
+				? res.json(user)
+				: res.status(400).json({ message: "Email ou senha incorreta" })
+		)
+		.catch(err => next(err))
+}
+
 // routes
 router.post("/register", register)
+router.post("/auth", auth)
 
 module.exports = router
