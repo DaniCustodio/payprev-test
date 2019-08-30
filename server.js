9724a9e74032daaa7a385d05782194c3a30d02a0
Danielle Custodio <dani.ps.custodio@gmail.com>
2019-08-30 09:38:24 -0300

    add /github route and errorHandler middleware

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 const express = require("express")
 const app = express()
+const errorHandler = require("./_helpers/error-handler")
 
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
 
 // api routes
 app.use("/users", require("./users/users-routes"))
+app.use("/github", require("./users-github/users-github-routes"))
+
+// global error handler
+app.use(errorHandler)
 
 const port = process.env.NODE_ENV || "3000"
 app.listen(port, () => {
