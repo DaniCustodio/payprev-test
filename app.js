6772ef65da6b032f261577545a872da4bc7083cc
Danielle Custodio <dani.ps.custodio@gmail.com>
2019-08-31 13:05:16 -0300

    separate app and server

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require("express")
+const app = express()
+const errorHandler = require("./_helpers/error-handler")
+
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
+
+// api routes
+app.use("/users", require("./users/users-routes"))
+app.use("/github", require("./users-github/users-github-routes"))
+app.use("/folders", require("./folders/folders-routes"))
+
+// global error handler
+app.use(errorHandler)
+
+module.exports = app
--- server.js
+++ server.js
@@ -1,17 +1,4 @@
-const express = require("express")
-const app = express()
-const errorHandler = require("./_helpers/error-handler")
-
-app.use(express.urlencoded({ extended: false }))
-app.use(express.json())
-
-// api routes
-app.use("/users", require("./users/users-routes"))
-app.use("/github", require("./users-github/users-github-routes"))
-app.use("/folders", require("./folders/folders-routes"))
-
-// global error handler
-app.use(errorHandler)
+const app = require("./app")
 
 const port = process.env.NODE_ENV || "3000"
 app.listen(port, () => {
